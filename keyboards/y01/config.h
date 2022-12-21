cb79b3b387c5af8c7e909f18e1155b8269d24127
Maxime Coirault <maxime.coirault@gmail.com>
2022-12-21 21:43:07 +0200

    [Keyboard] update y01 to latest qmk specs

--- keyboards/y01/config.h
+++ keyboards/y01/config.h
@@ -2,13 +2,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x592d /* "Y-" */
-#define PRODUCT_ID      0x3031 /* "01" */
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Yornn
-#define PRODUCT         Y-01
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
@@ -16,7 +9,6 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F0, F1, C7, D5, B7 }
 #define MATRIX_COL_PINS { B0, F4, F5, F6, F7, B1, B3, B2, B6, B5, B4, E6, D7, C6, D4 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
