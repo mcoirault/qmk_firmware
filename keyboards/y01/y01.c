6d1f6b339ee3ca7366f1f66680658ef116783f32
Maxime Coirault <maxime.coirault@gmail.com>
2023-09-25 13:04:32 +0300

    Y-01 - implement game of life

--- keyboards/y01/y01.c
+++ keyboards/y01/y01.c
@@ -1,5 +1,8 @@
 #include "y01.h"
 #include "bongo.h"
+#include "gol.h"
+
+bool bongo_displayed = true;
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
@@ -12,6 +15,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         case KC_RGUI:
             anim_timer = 99;  // forces an animation reset
             return true;
+        case KC_F22:
+            if (record->event.pressed) {
+                reset_gol();
+            }
+            return false;
+        case KC_F23:
+            if (record->event.pressed) {
+                bongo_displayed = !bongo_displayed;
+            }
+            return false;
         case KC_F24:
             if (record->event.pressed) {
                 current_default_anim = (current_default_anim + 1) % 3;
@@ -26,11 +39,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    srand(timer_read());
     return OLED_ROTATION_180;
 }
 
 bool oled_task_user(void) {
-    draw_bongo_dynamic();
+    if (bongo_displayed) {
+        draw_bongo_dynamic();
+    }
+    else {
+        draw_gol();
+    }
     return false;
 }
 #endif
