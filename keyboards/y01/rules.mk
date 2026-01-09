d7b1f75672b558196a1ae85532d74b15c0d67e8d
Maxime Coirault <maxime.coirault@gmail.com>
2026-01-09 18:50:57 +0200

    Y-01 - migrate info.json to keyboard.json

--- keyboards/y01/rules.mk
+++ keyboards/y01/rules.mk
@@ -1,20 +1,11 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 WPM_ENABLE = yes
-LTO_ENABLE = yes
 VIA_ENABLE = yes
 
-MOUSEKEY_ENABLE = no        # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
